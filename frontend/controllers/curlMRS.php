5554ff3fa86e4db06ec5497befdb4707396b8da3
hungnv_580 <hungnv_580@vnu.edu.vn>
2016-12-20 15:31:59 +0700

    Update curlMRS phân quyền người dùng

--- frontend/controllers/curlMRS.php
+++ frontend/controllers/curlMRS.php
@@ -8,17 +8,54 @@
 
 namespace frontend\controllers;
 
+use backend\models\ServiceAccess;
+use Yii;
+use yii\helpers\Url;
+
+
 
 class curlMRS
 {
+    public $username;
+    public $password;
+
+    public function getUser()
+    {
+        if (!Yii::$app->user->isGuest) {
+            $user = ServiceAccess::find()->where(['=','user_id',Yii::$app->user->identity->getId()])->asArray()->all();
+            if (!$user) {
+                echo "<script>alert('You don\'t have permission'');</script>";
+                $url = Url::to(['openmrs/null']);
+                header("Location: localhost: "); /* Redirect browser */
+                exit();
+            }
+            else {
+                $this->username = $user[0]['id3'];
+                $this->password = $user[0]['pw3'];
+            }
+        }
+        else {
+            return Yii::$app->response->redirect(Url::to(['openmrs/null']));
+        }
+    }
+
     public function get($url) {
+        $this->getUser();
+        $username = $this->username;
+        $password = $this->password;
+        if ($this->username =='' ){
+            return Yii::$app->response->redirect(Url::to(['openmrs/null']));
+        }
+        $user = $username.':'.$password;
+//        $user = '"'.$user.'"';
+//        var_dump($user);die;
         // Get cURL resource
         $curl = curl_init();
         // Set some options - we are passing in a useragent too here
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_URL => $url,
-            CURLOPT_USERPWD => "admin:Admin123",
+            CURLOPT_USERPWD => $user,
             CURLOPT_HTTPHEADER, array('Content-Type:application/json'),
             CURLOPT_USERAGENT => 'Codular Sample cURL Request'
         ));
