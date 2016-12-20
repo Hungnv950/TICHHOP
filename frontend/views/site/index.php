7b17244ac3c06b717b24a071628449d3228c3ea7
luuthang <luuthang2011@gmail.com>
2016-12-20 16:02:36 +0700

    description

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -18,21 +18,21 @@ $this->title = 'Trang chủ';
             <div class="col-lg-4">
                 <h2>Better Doctor</h2>
 
-                <p>Mô tả</p>
+                <p class="well">Hệ thống cung cấp cho người bệnh dữ liệu chính xác về bác sĩ và trung tâm dịch vụ y tế (giải pháp áp dụng tại Mỹ)</p>
 
                 <p><a class="btn btn-primary" href="<?= Yii::$app->getUrlManager()->baseUrl?>/doctor/index">Sử dụng</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>OrthanC</h2>
 
-                <p>Mô tả</p>
+                <p class="well">Truy cập và xem nội dung của các tập tin DICOM (hình ảnh ý tế), hỗ trợ chuẩn đoán và phân tích kết quả.</p>
 
                 <p><a class="btn btn-success" href="<?= Yii::$app->getUrlManager()->baseUrl?>/orthanc/index">Sử dụng</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Open MRS</h2>
 
-                <p>Mô tả</p>
+                <p class="well"><br>Hệ thống hồ sơ ý tế điện tử <br><br></p>
 
                 <p><a class="btn btn-danger" href="<?= Yii::$app->getUrlManager()->baseUrl?>/openmrs/index">Sử dụng</a></p>
             </div>
